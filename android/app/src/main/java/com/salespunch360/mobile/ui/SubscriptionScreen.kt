598cc93885ec3e415e257e2d85560281dcc9ef08
pradeep5849 <sales.amjconsterior@gmail.com>
2026-09-18 11:05:10 +0530

    Fix Android subscription screen syntax

--- android/app/src/main/java/com/salespunch360/mobile/ui/SubscriptionScreen.kt
+++ android/app/src/main/java/com/salespunch360/mobile/ui/SubscriptionScreen.kt
@@ -94,7 +94,6 @@ private fun SalesPurchase(data:MobileBillingContext,onQuote:(BillingQuote)->Unit
         }
     }
 }
-}
 @Composable
 private fun AccountPurchase(data:MobileBillingContext,onQuote:(BillingQuote)->Unit){
     val context=LocalContext.current
